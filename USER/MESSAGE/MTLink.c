844876047fc0584da463e12a86e858087c3549bd
smake <Mr_smake@hotmail.com>
2020-04-10 16:03:18 +0800

    2020_4_10__16_03

--- USER/MESSAGE/MTLink.c
+++ USER/MESSAGE/MTLink.c
@@ -1,28 +1,6 @@
 #include "MTLink.h"
 
-MTLink_typedef MTLink_UDP={0};
-QueueHandle_t MTLinkUDPAskQueue = NULL;
 
-#define MTLink_Send(buf,len)  udp_send(buf,len)    //定义发送宏
-
-/**
-  * @funNm   
-  * @brief   
-  * @param	 
-  * @retval 
-  */
-void MTLinkID_Init(void)
-{
-	uint8_t myid[2]={MY_ID,CAN_IMU_ID};
-	MTLinkSetMY_ID(&MTLink_UDP,myid,sizeof(myid));
-}
-	
-void MTLinkUDPAskQueue_Init(void)
-{
-	do{
-		MTLinkUDPAskQueue=xQueueCreate(1, sizeof(int));
-	}while(MTLinkUDPAskQueue == NULL);
-}
 
 /************需要修改********MTLink_UDP****************************************************/
 //根据目标ID修改发送函数
@@ -33,7 +11,7 @@ bool MTLink_SendBuffer(uint8_t DID,uint8_t *buf,int len)
 		case FREE_ID:
 			break;
 		case HOST_ID:
-			MTLink_Send(buf,len);
+			udp_send(buf,len);
 			break;
 		default:
 			break;
@@ -76,23 +54,4 @@ void MTLinkPrint(MTLink_typedef *m,uint8_t SID,uint8_t DID,char* str,int len,uin
 {
 	MTLink_Encode(&MTLink_UDP,SID,DID,0,0x0000,(uint8_t*)str,len,timeout);
 }
-
-void MTLinkPutChar(MTLink_typedef *m,uint8_t SID,uint8_t DID,char* str)
-{
-	int len=sizeof(str)+1;
-	MTLink_Encode(m,SID,DID,0,0x0000,(uint8_t*)str,len,100);
-}
-
-char MTprintbuf[512];
-void MTLinkPrintf(MTLink_typedef *m,uint8_t SID,uint8_t DID,const char * format, ... )
-{
- //va_list args;
- //va_start (args, format);
- //vsnprintf (MTprintbuf,sizeof(MTprintbuf)-1,format, args);
- int len0 = strlen((const char *)MTprintbuf);
- MTprintbuf[len0]='\0';
- MTLink_Encode(&MTLink_UDP,SID,DID,0,0x0000,(uint8_t*)MTprintbuf,len0+1,100);
- //va_end (args);  
-}
-
 
